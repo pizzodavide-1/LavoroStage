0eb2a7a98ce3d02165501511325dcc0fe55b6101
pizzodavide-1 <47659735+pizzodavide-1@users.noreply.github.com>
2019-09-12 15:40:39 +0200

    Add files via upload

---
+++ Asse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphicViewer
+{
+    class Asse
+    {
+        public string Unità { get; set; }
+        public string Dettaglio { get; set; }
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Origine { get; set; }
+    }
+}
